91e3526de740c36b51a47c602573f7140cae7922
narendran <nanil.mines@gmail.com>
2026-05-26 16:37:11 +0530

    Test

--- src/main/java/com/simplehearing/appointment/controller/AppointmentController.java
+++ src/main/java/com/simplehearing/appointment/controller/AppointmentController.java
@@ -1,20 +1,34 @@
 package com.simplehearing.appointment.controller;
 
-import com.simplehearing.appointment.dto.*;
-import com.simplehearing.appointment.service.AppointmentService;
-import com.simplehearing.auth.security.UserPrincipal;
-import com.simplehearing.common.dto.ApiResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.UUID;
+import com.simplehearing.appointment.dto.AppointmentResponse;
+import com.simplehearing.appointment.dto.BookAppointmentRequest;
+import com.simplehearing.appointment.dto.CreateSlotRequest;
+import com.simplehearing.appointment.dto.SlotResponse;
+import com.simplehearing.appointment.dto.UpdateAppointmentStatusRequest;
+import com.simplehearing.appointment.service.AppointmentService;
+import com.simplehearing.auth.security.UserPrincipal;
+import com.simplehearing.common.dto.ApiResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 
 @Tag(name = "Appointments", description = "Therapist availability slots and appointment booking")
 @RestController
@@ -24,6 +38,7 @@ public class AppointmentController {
     private final AppointmentService appointmentService;
 
     public AppointmentController(AppointmentService appointmentService) {
+        // dummy commit
         this.appointmentService = appointmentService;
     }
 
