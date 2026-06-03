cc9a78c6315b4516274c31f7be39c231aed64642
narendran <nanil.mines@gmail.com>
2026-06-03 14:01:41 +0530

    fix missing List import in EmailService

--- src/main/java/com/simplehearing/notification/EmailService.java
+++ src/main/java/com/simplehearing/notification/EmailService.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
